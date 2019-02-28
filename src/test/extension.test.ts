d5a26580f4437f64c59664c7bbb0cbff5427797f
Danny Tuppeny <danny@tuppeny.com>
2019-02-28 17:34:17 +0000

    Switch to using env.openExternal

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -5,18 +5,12 @@ import * as sinon from 'sinon';
 
 suite("Extension Tests", function () {
     test("functionToTest opens browser", function () {
-        // Intercept vscode.open so we don't spawn browsers!
-        const executeCommand = sinon.stub(vs.commands, "executeCommand").callThrough();
-        const open = executeCommand.withArgs("vscode.open", sinon.match.any).resolves();
-
         // Call the function to test.
         functionToTest();
 
-        // Ensure it attempted to open the browser.
-        assert.equal(open.called, true);
     });
 });
 
 function functionToTest() {
-    vs.commands.executeCommand("vscode.open", vs.Uri.parse("https://www.google.co.uk/"));
+    vs.env.openExternal(vs.Uri.parse("https://www.google.co.uk/"));
 }
